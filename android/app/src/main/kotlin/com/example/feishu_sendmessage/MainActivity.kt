4e61b95db4976f3f2d74effd793448f18da04a94
qq762538930 <762538930@qq.com>
2022-12-03 16:25:18 +0800

    codes

---
+++ android/app/src/main/kotlin/com/example/feishu_sendmessage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feishu_sendmessage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Retrieve Text Input',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  TextEditingController myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('发送飞书消息'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+              child: TextField(
+                controller: myController,
+                decoration: InputDecoration(
+                    labelText: '发送飞书文本消息',
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          myController.clear();
+                        });
+                      },
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // When the user presses the button, show an alert dialog containing
+        // the text that the user has entered into the text field.
+        onPressed: () {
+          // showDialog(
+          //   context: context,
+          //   builder: (context) {
+          //     return AlertDialog(
+          //       // Retrieve the text the that user has entered by using the
+          //       // TextEditingController.
+          //       content: Text(myController.text),
+          //     );
+          //   },
+          // );
+          sendMessage();
+          // print(myController.text);
+        },
+        tooltip: '发送',
+        child: const Icon(Icons.send),
+      ),
+    );
+  }
+
+  Future<void> sendMessage() async {
+    String data = myController.text;
+    var headers = {
+      'User-Agent':
+      'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)'
+    };
+    var request = http.Request(
+        'POST',
+        Uri.parse(
+            'https://open.feishu.cn/open-apis/bot/v2/hook/a2eff83d-3089-4900-b929-5f10f2fd6ff1?msg_type=text&content={"text":"$data"}'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
+    // print(myController.text);
+  }
+}
